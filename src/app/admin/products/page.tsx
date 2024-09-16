a25300d54988f20c6cd8cb020aafdc43c1c51e06
ericstober <erictstober@gmail.com>
2024-09-16 15:29:50 -0500

    add products page and PageHeader component

---
+++ src/app/admin/products/page.tsx
@@ -0,0 +1,9 @@
+import { PageHeader } from "../_components/PageHeader";
+
+export default function AdminProductsPage() {
+  return (
+    <>
+      <PageHeader>Products</PageHeader>
+    </>
+  );
+}
