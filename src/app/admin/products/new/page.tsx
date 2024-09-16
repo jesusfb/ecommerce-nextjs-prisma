82a6d95b131fd778c35065ff62b587e19a862f6f
ericstober <erictstober@gmail.com>
2024-09-16 15:50:53 -0500

    moved PageHeader component and created new product page

---
+++ src/app/admin/products/new/page.tsx
@@ -0,0 +1,10 @@
+import { PageHeader } from "@/components/PageHeader";
+
+export default function NewProductPage() {
+  return (
+    <>
+      <PageHeader>Add Product</PageHeader>
+      <ProductForm />
+    </>
+  );
+}
--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import { PageHeader } from "../_components/PageHeader";
+import { PageHeader } from "@/components/PageHeader";
 import Link from "next/link";
 import { Table, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 
